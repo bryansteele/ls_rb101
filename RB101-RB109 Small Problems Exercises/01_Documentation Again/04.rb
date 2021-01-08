1efa91ed9505a997b8e3743fb7e6c1b7d20df0a4
bryansteele <bryan@steele101.com>
2021-01-07 22:15:44 -0500

    Completes Docs Ex#4

--- RB101-RB109 Small Problems Exercises/01_Documentation Again/04.rb
+++ RB101-RB109 Small Problems Exercises/01_Documentation Again/04.rb
@@ -1,1 +1,5 @@
 a = [1, 4, 8, 11, 15, 19]
+p a.bsearch {|x| x > 8}
+
+# We use the 'Find-Minimum Mode' binary search.
+# In *find-minimum mode*, method `bsearch` **returns the first element for which the block returns true**.
