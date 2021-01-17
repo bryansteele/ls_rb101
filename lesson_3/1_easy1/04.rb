f5571fdc62963254d1bcc893d0b087dba847959d
bryansteele <bryan@steele101>
2021-01-16 19:22:21 -0500

    Completes Question 4

---
+++ lesson_3/1_easy1/04.rb
@@ -0,0 +1,18 @@
+numbers = [1, 2, 3, 4, 5]
+
+p numbers.delete_at(1)
+
+p numbers
+# => 2
+# Returns deleted element or nil if out of range
+# Deletes the element from self at index given
+
+
+numbers = [1, 2, 3, 4, 5]
+
+p numbers.delete(1)
+
+p numbers
+# 1
+# Returns element deleted or nil if not in array.
+# Deletes element from self if given value matches 
