550798b8ea1b16165837df15428d8429424ff7d2
bryansteele <bryan@steele101.com>
2021-01-18 15:58:15 -0500

    Completes #7

---
+++ lesson_3/3_medium1/07.rb
@@ -0,0 +1,20 @@
+munsters = {
+  "Herman" => { "age" => 32, "gender" => "male" },
+  "Lily" => { "age" => 30, "gender" => "female" },
+  "Grandpa" => { "age" => 402, "gender" => "male" },
+  "Eddie" => { "age" => 10, "gender" => "male" },
+  "Marilyn" => { "age" => 23, "gender" => "female"}
+}
+
+def mess_with_demographics(demo_hash)
+  demo_hash.values.each do |family_member|
+    family_member["age"] += 42
+    family_member["gender"] = "other"
+  end
+end
+
+mess_with_demographics(munsters)
+
+puts munsters
+
+# It DID change the original data. The method definition added 42 to all of their ages and changed everyone's gender to other. All because []= is not reasigning, it is modifying the actual hash object.
