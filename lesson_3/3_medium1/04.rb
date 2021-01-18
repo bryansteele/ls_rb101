de2ea06a2376a4a13a38f93f56090eb26bc59647
bryansteele <bryan@steele101.com>
2021-01-18 15:36:00 -0500

    Completes #4

---
+++ lesson_3/3_medium1/04.rb
@@ -0,0 +1,14 @@
+def rolling_buffer1(buffer, max_buffer_size, new_element)
+  buffer << new_element
+  buffer.shift if buffer.size > max_buffer_size
+  buffer
+end
+
+def rolling_buffer1(input_array, max_buffer_size, new_element)
+  buffer = input_array + [new_element]
+  buffer.shift if buffer.size > max_buffer_size
+  buffer
+end
+
+# They will have the same return value, but the first method definition's argument buffer will be modified from uysing the <<.
+# The second method definition will not modify the caller's input argument.
