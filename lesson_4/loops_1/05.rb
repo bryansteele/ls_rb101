474d2181adbd827240ee83371d67c16ef16ed413
bryansteele <bryan@steele101.com>
2021-01-22 15:27:09 -0500

    Completes all of Loop1 Exercises

--- lesson_4/loops_1/05.rb
+++ lesson_4/loops_1/05.rb
@@ -1,6 +1,8 @@
-say_hello = 5
+say_hello = true
+counter = 0 
 
-while say_hello > 0
+while say_hello
   puts 'Hello!'
-  say_hello -= 1
+  counter += 1
+  say_hello = false if counter == 5
 end
---
+++ lesson_4/loops_1/08.rb
@@ -0,0 +1,7 @@
+numbers = [7, 9, 13, 25, 18]
+
+num = 0
+until num == numbers.length
+  puts numbers[num]
+  num += 1
+end
