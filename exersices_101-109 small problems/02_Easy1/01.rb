59722c9c2c3b8aadaf4cd0039176432a34412006
bryansteele <bryan@steele101.com>
2021-01-30 17:19:13 -0500

    Completes 1 & 2

--- exersices_101-109 small problems/02_Easy1/01.rb
+++ exersices_101-109 small problems/02_Easy1/01.rb
@@ -1,5 +1,18 @@
-def repeat(str, num)
-  num.times {puts str}
+# Input: 1 string & 1 integer
+# Output: String - 1 or more times
+#                - the number of times the string is printed to the screen it the 
+#                  number given as an argument.
+
+
+def repeat(string, integer)
+    integer.times { puts string }
 end
 
+
+
+
 repeat('Hello', 3)
+
+# Hello
+# Hello
+# Hello
--- exersices_101-109 small problems/02_Easy1/02.rb
+++ exersices_101-109 small problems/02_Easy1/02.rb
@@ -1,10 +1,27 @@
-def is_odd?(int)
-  int % 2 != 0
+# input: 1 integer
+# output: boolean
+
+# - integer passed in can be  +, - or 0.
+
+# - If the integer's absolute value is odd. return true
+# - We are assuming the arghument is a valid int without varifying.
+#  From he test cases, we will return false if the absolute is not odd.
+
+# absolute of -4 is 4 
+# absolute of 4 is 4      --- the number of units from zero.
+#  Absolutes will alwasy be a + number.
+
+# if NUM's absolute is odd
+# then return true
+# else return false
+
+def is_odd?(num)
+  num.abs % 2 != 0 ? true : false
 end
 
-puts is_odd?(2)    
-puts is_odd?(5)   
-puts is_odd?(-17) 
-puts is_odd?(-8)   
-puts is_odd?(0)    
-puts is_odd?(7)
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
