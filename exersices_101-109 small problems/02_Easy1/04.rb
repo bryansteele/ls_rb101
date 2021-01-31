db07f4def8549a7735de4a7be700b84eff74b8ad
bryansteele <bryan@steele101.com>
2021-01-30 19:28:36 -0500

    Starts number 4

---
+++ exersices_101-109 small problems/02_Easy1/04.rb
@@ -0,0 +1,20 @@
+# Write a method that counts the number of occurrences of each element in a given array.
+
+# input: 1 array of strings
+# output: hash - vehical type as key, and the number of occurenses in array as the values.
+
+
+
+vehicles = [
+    'car', 'car', 'truck', 'car', 'SUV', 'truck',
+    'motorcycle', 'motorcycle', 'car', 'truck'
+]
+
+count_occurrences(vehicles)
+
+
+
+# car => 4
+# truck => 3
+# SUV => 1
+# motorcycle => 2
