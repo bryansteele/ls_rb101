d3a1c2677e124411508c28d11788be42d7c845d7
bryansteele <bryan@steele101.com>
2021-02-02 12:13:57 -0500

    Completes Ex #4

--- exersices_101-109 small problems/02_Easy1/04.rb
+++ exersices_101-109 small problems/02_Easy1/04.rb
@@ -6,25 +6,27 @@
 # 
 
 vehicles = [
-    'car', 'car', 'truck', 'car', 'SUV', 'truck',
-    'motorcycle', 'motorcycle', 'car', 'truck'
+    'car', 'car', 'truck', 'Car', 'SuV', 'truCk',
+    'moTorcycle', 'motorcYcle', 'car', 'Truck'
 ]
 
 def count_occurrences(vehicle_list)
-  vehicle_occurrences = {}
+  occurenses_hsh = {}
   
-  vehicle_list.uniq.each do |vehicle|
-    vehicle_occurrences[vehicle] = vehicle_list.count(vehicle)
+  downcase_list = vehicle_list.map do |x|
+                    x.downcase
+                  end
+
+  downcase_list.uniq.each do |i|
+    occurenses_hsh[i] = downcase_list.count(i)
   end
 
-  vehicle_occurrences.each do |k, v|
+  occurenses_hsh.each do |k, v|
     puts "#{k} => #{v}"
   end
 end
 
 count_occurrences(vehicles)
-
-
 # car => 4
 # truck => 3
 # SUV => 1
