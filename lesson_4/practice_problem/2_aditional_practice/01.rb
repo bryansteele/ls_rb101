62b8379d41d097bfc1fc57361e0bd82251a2edbd
bryansteele <bryan@steele101.com>
2021-02-11 21:58:14 -0500

    completes Ex 1 review

--- lesson_4/practice_problem/2_aditional_practice/01.rb
+++ lesson_4/practice_problem/2_aditional_practice/01.rb
@@ -1,21 +1,16 @@
 flintstones = ["Fred", "Barney", "Wilma", "Betty", "Pebbles", "BamBam"]
 
-# input: array
-# output: hash
-
-# rules: 
-# - the values of the array elements become the keys to the hash
-# - hash values will be the array position of each name
-#     - { "Fred" => 1, "Barney" => 2 ....}
-# Does position in the array mean index or 1st, 2nd, 3rd, etc?
-
-# Below I am adding 1 to the index to make it in the 1st, 2nd. 3rd .... instead of position 0, 1, 2, 3... 
-
-
-hash = {}
+hsh = {}
 flintstones.each_with_index do |name, idx|
-  hash[name] = idx + 1
+  hsh[name] = idx
 end
 
-p hash
-p flintstones
+p hsh
+
+
+
+
+
+
+
+
