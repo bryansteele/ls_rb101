5a4e28b44b5d61218237cb2f514435bf55ca0a53
bryansteele <bryan@steele101.com>
2021-02-16 22:13:56 -0500

    Completes #8

--- lesson_5/practice_problems/08.rb
+++ lesson_5/practice_problems/08.rb
@@ -1,8 +1,19 @@
 hsh = {first: ['the', 'quick'], second: ['brown', 'fox'], third: ['jumped'], fourth: ['over', 'the', 'lazy', 'dog']}
 
 hsh.values.each do |arr|
-  arr.flatten.each_with_index do |letter, word|
-    p word
-    p letter
+  arr.flatten.each do |word|
+    word.chars.each do |letter|
+      puts letter.scan(/[aeoui]/)
+    end
   end
 end
+
+# vowels = 'aeiou'
+
+# hsh.each do |_, value|
+#   value.each do |str|
+#     str.chars.each do |char|
+#       puts char if vowels.include?(char)
+#     end
+#   end
+# end
