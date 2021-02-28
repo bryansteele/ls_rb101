9d8aec2fa5aecb7d79b808cadcca4cc7f11c62d3
bryansteele <bryan@steele101.com>
2021-02-28 18:27:42 -0500

    *

--- lesson_6/assignments/ttt.rb
+++ lesson_6/assignments/ttt.rb
@@ -1,6 +1,3 @@
-# require 'yaml'
-
-# MESSAGES = YAML.load_file('ttt_msgs.yml')
 WINNING_LINES = [
   [1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7],
   [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]
@@ -13,10 +10,6 @@ COMPUTER_MARKER = 'O'
 def clear_screen
   system('clear') || system('clr')
 end
-
-# def messages(msg)
-#   MESSAGES[msg]
-# end
 
 def prompt(msg)
   puts "➣  #{msg}"
@@ -72,7 +65,7 @@ def first_player_prompt
   puts "⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼"
   prompt "Who goes first?"
   puts "⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼"
-  sleep(1)
+  sleep(2)
   clear_screen
   puts "⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼"
   prompt "You or the Me? ENTER (C)omputer or (P)layer:"
@@ -80,7 +73,7 @@ def first_player_prompt
 end
 
 def valid_first_player?(choice)
-  %w(c p).include?(choice.downcase)
+  %w(c p).include?(choice)
 end
 
 def retreive_first_player
@@ -88,12 +81,16 @@ def retreive_first_player
   first_player_prompt
   
   loop do
-    answer = gets.chomp
+    answer = gets.chomp.downcase
     break if valid_first_player?(answer)
     puts "Invalid Input! Please ENTER 'C' or 'P'"
   end
 
-  answer
+  first_player(answer)
+end
+
+def first_player(first_to_play)
+  first_to_play == 'c' ? 'computer' : 'player'
 end
 
 def initialize_score
@@ -108,7 +105,7 @@ end
 
 # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
 def display_gameboard(brd)
-  clear_screen
+  clear_screen unless empty_squares(brd) == 9
   puts "⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼"
   puts "🔹You are: #{PLAYER_MARKER}   🔸I am: #{COMPUTER_MARKER}".center(44)
   puts "⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼"
@@ -132,8 +129,6 @@ def display_gameboard(brd)
   puts "               𝟟.|         𝟠.|         𝟡."
   puts ""
   puts "⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼"
-  # puts "YOURE SCORE: #{scores[:player]}     My SCORE: #{scores[:computer]}"
-  # puts "⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼"
 end
 # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
@@ -152,24 +147,28 @@ def joinor(arr, delimiter=', ', word='or')
   end
 end
 
-# def place_piece!(brd, player)
-#   if player == "player"
-#     player_places_piece!(brd)
-#   else
-#     computer_places_piece!(brd)
-#   end
-# end
+def the_play!(current_player, brd)
+  if current_player == "player"
+    player_turn!(brd)
+  else
+    computer_turn!(brd)
+  end
+end
 
 def player_turn!(brd)
   square = ''
   loop do
-    puts "🔷 Choose a square #{joinor(empty_squares(brd))} 🔶"
+    puts "🔷 Choose a square #{joinor(empty_squares(brd))}"
     square = gets.chomp.to_i
     break if empty_squares(brd).include?(square)
     prompt "Sorry, that's not a valid choice."
   end
 
   brd[square] = PLAYER_MARKER
+end
+
+def alternating_players(player)
+  player == 'player' ? 'computer' : 'player'
 end
 
 def computer_turn!(brd)
@@ -181,28 +180,74 @@ def board_full?(brd)
   empty_squares(brd).empty?
 end
 
-# def valid_win?(brd)
-#   if !!detect_winner(brd)
-#     prompt "#{detect_winner(gameboard)} won!"
-#   else
-#     prompt "It's a tie!"
-#   end
-
-# end
-
-def detect_winner(brd)
+def valid_win_of_round(brd)
+  winner_of_round = ''
   WINNING_LINES.each do |line|
     if brd.values_at(*line).count(PLAYER_MARKER) == 3
-      return 'Player'
+      winner_of_round = 'Player'
     elsif brd.values_at(*line).count(COMPUTER_MARKER) == 3
-      return 'Computer'
+      winner_of_round =  'Computer'
+    else
+      winner_of_round = 'tie'
     end
   end
 
-  nil
+  winner_of_round
 end
+
+def display_winner_of_round(winner_of_round)
+  case winner_of_round
+  when 'player'   then prompt "You won that round."
+  when 'computer' then prompt "I won this round."
+  else                 prompt "TIE! Let's do this again."
+  end
+end
+
+def increment_score(round_winner, scores)
+  scores[:player] += 1   if round_winner == 'player'
+  scores[:computer] += 1 if round_winner == 'computer'
+end
+
+def display_scoreboard(scores)
+  puts "🔹YOURE SCORE: #{scores[:player]}     🔸My SCORE: #{scores[:computer]}".center(44)
+  puts "⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼⎼"
+end
+
+def game_over?(scores)
+  scores[:player] == WINNING_MATCH || scores[:computer] == WINNING_MATCH
+end
+
+def establish_grand_winner(scores)
+  if scores[:player] == WINNING_MATCH && scores[:computer] != WINNING_MATCH
+    grand_winner = true
+  elsif scores[:computer] == WINNING_MATCH || scores[:player] != WINNING_MATCH
+    grand_winner = false
+  end
+
+  grand_winner
+end
+
+def display_end_of_game
+  prompt "GAME OVER!"
+end
+
+def display_grand_winner(winner)
+  if winner
+    prompt "YOU ARE the GRAND WINNER! CONGRATULATIONS!\n "
+  else
+    prompt "I AM the GRAND WINNER! Better Luck Next Time.\n \n "
+  end
+end
+
+def play_again?
+  clear_screen
+  prompt "Would you like to play again? (Y/y to play again):"
+  gets.chomp
+end
+
 # #########################################################################
 # BEGINNING
+winner_of_round = ''
 quit_str = ''
 clear_screen
 display_instructional_greeting
@@ -213,26 +258,33 @@ clear_screen
 # MAIN LOOP
 while quit_str == ''
   scoreboard = initialize_score
-  first_player = retreive_first_player
-  
-  # loop do
   gameboard = initialize_gameboard
-  display_gameboard(gameboard)
-  player_turn!(gameboard)
-  break if someone_won?(gameboard) || board_full?(gameboard)
+  current_player = retreive_first_player
 
-  computer_turn!(gameboard)
-  break if someone_won?(gameboard) || board_full?(gameboard)
-  display_gameboard(gameboard)
+  loop do
+      display_gameboard(gameboard)
+      display_scoreboard(scoreboard)
+      the_play!(current_player, gameboard)
+      current_player = alternating_players(current_player)
+      break if valid_win_of_round(gameboard) || board_full?(gameboard)
 
-  # winner_of_round = valid_win?(gameboard)
-  # display_winner_of_round(winner_of_round)
-  # increment_score(winner_of_round, scoreboard)
-  # isplay_incrementing_score(scoreboard)
+    loop do
+    display_gameboard(gameboard)
+    # display_scoreboard(scoreboard)
+    winner_of_round = valid_win_of_round(gameboard)
+    display_winner_of_round(winner_of_round)
+    increment_score(winner_of_round, scoreboard)
+    break if game_over?(scoreboard)
+    end
 
-
-  prompt "Play again? (y or n)"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  grand_winner = establish_grand_winner(scoreboard)
+  clear_screen
+  display_end_of_game
+  display_grand_winner(grand_winner)
+  another_round = play_again?
+  break if another_round.downcase != 'y'
+  end
 end
+
+clear_screen
 prompt "GOOD BYE!"
